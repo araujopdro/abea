0c5b1a76f11192a5c317bf196d9da930debf1da2
Pedro <pedroaraujo@morethanreal.io>
2020-11-06 00:22:28 -0300

    css

--- index.php
+++ index.php
@@ -628,6 +628,27 @@
 				margin: 0;
 			    background-color: white;
 			}
+			.caracteristicas-tooltip-holder{
+				position: relative;
+			    display: inline-block;
+			    border-bottom: 1px dotted black;
+			}
+
+			.caracteristicas-tooltip-holder .caracteristicas-tooltip-text {
+				visibility: hidden;
+				width: 120px;
+				background-color: black;
+				color: #fff;
+				text-align: center;
+				padding: 5px 0;
+				border-radius: 6px;
+
+				position: absolute;
+				z-index: 1;
+			}
+			.caracteristicas-tooltip-holder:hover .caracteristicas-tooltip-text {
+				visibility: visible;
+			}
 		</style>
 	</head>
 	<body> 
