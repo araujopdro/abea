de8e1f2979f3072e19d6b51b41d24e2bbde5dc74
Pedro <pedroaraujo@morethanreal.io>
2020-11-19 11:18:32 -0300

    fix structure

--- create.php
+++ create.php
@@ -10,7 +10,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		
 		<link rel="shortcut icon" type="image/png" href="imgs/favicon.png"/>
-		
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   		<link rel="stylesheet" href="css/create.css">
 	</head>
 	<body> 
@@ -146,7 +146,6 @@
 
 		<script src="https://kit.fontawesome.com/3f043c6910.js" crossorigin="anonymous"></script>
 		<script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 		<script src="js/refs.js"></script>
--- index.php
+++ index.php
@@ -8,15 +8,14 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<script src="https://kit.fontawesome.com/3f043c6910.js" crossorigin="anonymous"></script>
-		<script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+
 		<link rel="shortcut icon" type="image/png" href="imgs/favicon.png"/>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  		<link rel="stylesheet" href="css/index.css">
 
 		<script type="text/javascript">
 			$( document ).ready(function() {
-		      	$.get("navbar.html", function (data) {
+		      	$.get("modeules_html/navbar.html", function (data) {
                     $("#main").append(data);
                     var nav_active = $("#nav-home");
                     nav_active.addClass("active");
@@ -42,14 +41,20 @@
 			});
 		</script>
 
-  		<link rel="stylesheet" href="index.css">
 
 	</head>
 	<body> 
 		<div style="width: 100%;position: fixed;top: 0;" id="header">
 			<div id="main" style="width: 45vw;margin: auto;">
     	
-    </div>
+    		</div>
 		</div>
+
+
+
+		
+		<script src="https://kit.fontawesome.com/3f043c6910.js" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 	</body>
 </html>
